a14c8b65fe0c03c9f58f6b36be05f010dc849ee7
Vikramjeet singh Singh <30014464@student.toiohomai.ac.nz>
2019-06-12 12:38:55 +1200

    msg

---
+++ Qustion 2/Qustion 2/Program.cs
@@ -0,0 +1,67 @@
+﻿// Name: Vikramjeet singh
+//Qustion: 2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qustion_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("**************Welcome To Holiday's Park Employee Payroll System**************\n\n");  
+            Console.Write("Please enter the employee's Full name: ");
+            string full_Name = Console.ReadLine();
+            Console.Write($"Please enter tax rate: ");
+            double tax = double.Parse(Console.ReadLine());
+            Console.Write($"Please enter Gross annual income: ");
+            double gross = double.Parse(Console.ReadLine());
+            Employee p1 = new Employee(full_Name, tax, gross);
+
+            //accessing person 1 Employee class object  
+            Console.WriteLine($"Name: {p1.Full_Name}\nTax: {p1.Tax}%\nGross Salary: {p1.Gross}\nNet Salary: {p1.Calculate_Net_Income()}");
+            Console.ReadLine();
+
+        }
+    }
+
+    class Employee
+    {
+        /*variables;*/
+        private string full_Name;
+        private double tax_rate;
+        private double gross;
+
+        /*Getters and Setters*/
+        public string Full_Name { get { return full_Name; } set { full_Name = value; } }
+        public double Tax { get { return tax_rate; } set { tax_rate = value; } }
+        public double Gross { get { return gross; } set { gross = value; } }
+
+        /*Constructors*/
+        public Employee()
+        {
+            Console.WriteLine("Employee Record created");
+        }
+
+
+        public Employee(string _full_Name, double _Tax_rate, double _Gross)
+        {
+            full_Name = _full_Name;
+            Tax = _Tax_rate;
+            Gross = _Gross;
+
+        }
+
+        /*Method */
+        public double Calculate_Net_Income()
+        {
+            return Gross * (1 - Tax / 100);
+        }
+    }
+}
+ 
+ 
+     
---
+++ qustion 3/qustion 3/Program.cs
@@ -0,0 +1,84 @@
+﻿//Name: Vikramjeet Singh
+//Student ID: 30014464
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qustion_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            Random rand = new Random();  //created Random class object
+
+
+            int[,] matrixA = new int[3, 3];
+            int[,] matrixB = new int[3, 3];
+            int[,] matrixC = new int[3, 3];
+
+
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    matrixA[i, j] = rand.Next(-10, 10);
+                    matrixB[i, j] = rand.Next(-10, 10);
+                }
+
+            }
+
+            /*For Loop to Display  Matrix A */
+            Console.Write("\nRandom 3x3 Matrix A:\n");
+            for (i = 0; i < matrixA.GetLength(0); i++)
+            {
+                Console.Write("\n");
+                for (j = 0; j < matrixA.GetLength(1); j++)
+                {
+                    Console.Write("\t{0}", matrixA[i, j]);
+                }
+
+            }
+
+            /*For Loop to Display  Matrix B */
+            Console.Write("\n\nRandom 3x3 Matrix B:\n");
+            for (i = 0; i < matrixA.GetLength(0); i++)
+            {
+                Console.Write("\n");
+                for (j = 0; j < matrixA.GetLength(1); j++)
+                {
+                    Console.Write("\t{0}", matrixB[i, j]);
+                }
+
+            }
+
+            /*For Loop to  ADD C=A+B Matrices  */
+            for (i = 0; i < matrixC.GetLength(0); i++)
+            {
+                for (j = 0; j < matrixC.GetLength(1); j++)
+                {
+                    matrixC[i, j] = matrixA[i, j] + matrixB[i, j];
+                }
+            }
+
+
+            /*Display Result of  two added matrices(C=A+B):*/
+            Console.Write("\n\nAddition Result of  two matrices C=A+B: \n");
+            for (i = 0; i < matrixC.GetLength(0); i++)  // row
+            {
+                Console.Write("\n");
+                for (j = 0; j < matrixC.GetLength(1); j++) //cloum
+                {
+                    Console.Write("\t{0}", matrixC[i, j]);
+                }
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
